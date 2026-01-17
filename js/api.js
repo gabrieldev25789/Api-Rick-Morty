ac474e195b75ece63b19455b2d7fab0505d90fcb
Gabriel <gabrielsb1090@gmail.com>
2026-01-17 02:22:06 -0300

    Adicionando ID e ajustando detalhes

--- js/api.js
+++ js/api.js
@@ -5,48 +5,59 @@ const episodeInfos = document.querySelector("#infos")
 async function consomeApi() {
 
   button.addEventListener("click", async () => {
+    if(input.value === ""){
+        episodeInfos.textContent = "Search for an apisode"    
+    return
+    }
     const inputValue = input.value
 
     const url = `https://rickandmortyapi.com/api/episode/${inputValue}`
 
     try {
+      episodeInfos.innerHTML = ""  
       const response = await fetch(url)
       const data = await response.json()
       console.log(data)
 
-      input.value = ""
+    input.value = ""
 
-      const ulInfos = document.createElement("ul")
-      ulInfos.id = "ul-infos"
+    const ulInfos = document.createElement("ul")
+    ulInfos.id = "ul-infos"
 
     const nameEpisode = document.createElement("li")
     nameEpisode.classList.add("list")
     nameEpisode.id = "name-episode"
-    nameEpisode.textContent = data.name 
+    nameEpisode.textContent = `Name: ${data.name}` 
 
     const dateRealese = document.createElement("li")
     dateRealese.classList.add("list")
     dateRealese.id = "date-realese"
-    dateRealese.textContent = data.air_date
+    dateRealese.textContent = `Date realese: ${data.air_date}`
 
     const episodeSeason = document.createElement("li")
     episodeSeason.classList.add("list")
     episodeSeason.id = "episode-season"
-    episodeSeason.textContent = data.episode
+    episodeSeason.textContent = `Season/Episode: ${data.episode}`
+
+    const id = document.createElement("li")
+    id.classList.add("list")
+    id.id = "episode-season"
+    id.textContent = `ID: ${data.id}`
 
     ulInfos.appendChild(nameEpisode)
     ulInfos.appendChild(dateRealese)
     ulInfos.appendChild(episodeSeason)
+    ulInfos.appendChild(id)
 
     episodeInfos.appendChild(nameEpisode)
     episodeInfos.appendChild(dateRealese)
     episodeInfos.appendChild(episodeSeason)
+    episodeInfos.appendChild(id)
 
       console.log(episodeInfos)
     } catch (error) {
       console.error("Erro ao buscar episódio", error)
     } 
-    
   })
 }
 
