0bd636066e65a1a5cef4d7968a3e07c8588d9f40
Gabriel <gabrielsb1090@gmail.com>
2026-02-05 19:01:05 -0300

    Estrutura inicial de filtros

--- js/select.js
+++ js/select.js
@@ -4,14 +4,47 @@ const specieSelect = document.querySelector("#species-select")
 const locationSelect = document.querySelector("#location-type-select")
 const dimensionSelect = document.querySelector("#dimension-select")
 
+const pageValue = Math.ceil(Math.random() * 10) 
+
+
+
+function createInfosCharactersSelect(data){
+    return [
+    createLi(`Name: ${data.name}`, "list", "name-location"),
+    createLi(`Dimension: ${data.species}`, "list", "species"),
+    createLi(`Type: ${data.gender}`, "list", "gender"),
+    createLi(`ID: ${data.id}`, "list", "id")
+    ]
+}
 
 async function handleSelects(){
-    const url = `https://rickandmortyapi.com/api/character`
+    const url = `https://rickandmortyapi.com/api/character?page=${pageValue}`
 
     const responseApi = await fetch(url)
     const data = await responseApi.json()
 
-    console.log(data.results)
+    const characters = data.results
+
+    statusSelect.addEventListener("change", (e) =>{
+        const target = e.target.value 
+
+    if(target === "alive"){
+        const characterAlive = characters.filter((character) => character.status === "Alive")
+        console.log(characterAlive)
+        characterAlive.forEach((character)=>{
+        const div = createDivImgInfos()
+        const img = createImg()
+        const ul = createUl() 
+        img.src = character.image 
+        createInfosCharactersSelect(character).forEach((li) => ul.appendChild(li))
+        div.appendChild(img)
+        div.appendChild(ul)
+        document.body.appendChild(div)
+        })
+    }
+})
+
+    console.log(data.results, pageValue)
 }
 
 handleSelects()
