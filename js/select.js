4a2a3a49c1729fb4813ad6b312eb32bf476560c2
Gabriel <gabrielsb1090@gmail.com>
2026-02-06 14:29:50 -0300

    Adicionando mais informações/adicionando classes

--- js/select.js
+++ js/select.js
@@ -4,18 +4,21 @@ const specieSelect = document.querySelector("#species-select")
 const locationSelect = document.querySelector("#location-type-select")
 const dimensionSelect = document.querySelector("#dimension-select")
 
-const pageValue = Math.ceil(Math.random() * 10) 
-
+const pageValue = Math.ceil(Math.random() * 42) 
 
 
 function createInfosCharactersSelect(data){
     return [
-    createLi(`Name: ${data.name}`, "list", "name-location"),
-    createLi(`Dimension: ${data.species}`, "list", "species"),
-    createLi(`Type: ${data.gender}`, "list", "gender"),
-    createLi(`ID: ${data.id}`, "list", "id")
+    createLi(`🧬 Name: ${data.name}`, "list-select", "name-location"),
+    createLi(`👽 Specie: ${data.species}`, "list-select", "species"),
+    createLi(`⚧ Gender: ${data.gender}`, "list-select", "gender"),
+    createLi(`📍 Location: ${data.location.name}`, "list-select", "location"),
+    createLi(`🌌 Origin: ${data.origin.name}`, "list-select", "origin"),
+    createLi(`🆔 ID: ${data.id}`, "list-select", "id")
     ]
 }
+
+let array = []
 
 async function handleSelects(){
     const url = `https://rickandmortyapi.com/api/character?page=${pageValue}`
@@ -32,14 +35,26 @@ async function handleSelects(){
         const characterAlive = characters.filter((character) => character.status === "Alive")
         console.log(characterAlive)
         characterAlive.forEach((character)=>{
+
         const div = createDivImgInfos()
+        div.classList.add("div-character-select")
+
         const img = createImg()
+        img.classList.add("img-character-select")
+        img.classList.remove("character-img")
+
         const ul = createUl() 
+        ul.classList.add("ul-character-select")
+
         img.src = character.image 
         createInfosCharactersSelect(character).forEach((li) => ul.appendChild(li))
         div.appendChild(img)
         div.appendChild(ul)
         document.body.appendChild(div)
+        array.push(div)
+
+        toggleClassList("add", selectsContainer, inputsContainer)
+        backBtn.classList.remove("hide")
         })
     }
 })
